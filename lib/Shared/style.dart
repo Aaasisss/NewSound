f4f040f534bf3305ee56ab3dfd221a7fa194130b
aaasisss <ashischaudhary959@gmail.com>
2022-04-12 15:38:08 +1000

    firebase connected to the app, modified pod and info list files and updated pods

---
+++ lib/Shared/style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Map<String, Color> fontColors = {
+  'theme': Color(0xFFFDB301),
+  'black-text': Color(0xFF202020),
+  'white-text': Color(0xFFE6E6E6),
+};
+
+Map<String, TextStyle> fontStyles = {
+  'title': TextStyle(fontFamily: "${GoogleFonts.carme()}"),
+  'general': TextStyle(fontFamily: "${GoogleFonts.montserrat()}"),
+};
